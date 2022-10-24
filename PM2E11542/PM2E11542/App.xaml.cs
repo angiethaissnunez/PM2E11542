7b337f16312800f658dadd7b38a79eb257bfa7cf
angie nunez <angiethaiss52@gmail.com>
2022-10-23 21:13:57 -0600

    Add project files.

---
+++ PM2E11542/PM2E11542/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PM2E11542.Controllers;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E11542
+{
+    public partial class App : Application
+    {
+
+        static BDsitios db;
+
+        public static BDsitios DBaseSitios
+        {
+            get
+            {
+                if (db == null)
+                {
+                    String folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PM2E11542.db3");
+
+                    db = new BDsitios(folderPath);
+                }
+
+                return db;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
